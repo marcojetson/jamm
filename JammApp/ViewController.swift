4ab409ca5a244d9376f36d2b0c0c064888345ec0
marco <marcojetson@gmail.com>
2016-09-03 14:31:58 +0200

    Initial Commit

---
+++ JammApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JammApp
+//
+//  Created by Marco Palmieri on 03/09/16.
+//  Copyright © 2016 Marco Palmieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
